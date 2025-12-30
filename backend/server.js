4b9195702e46dea66c00824f011204cd1312fc0a
TestSubject71 <ethosislami07@gmail.com>
2025-12-30 13:25:03 +0700

    ADD - Server & ENV

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config()
+
+// init app
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(()=> console.log("MongoDB Connected Successfully"))
+    .catch((err) => console.log("Error : ", err));
+
+
+app.get('/', (req, res) => {
+  res.send('Server is up and running!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
