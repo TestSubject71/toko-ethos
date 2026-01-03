e3b0047ffdff67f1c8870a31d7cc3e28fff387ab
TestSubject71 <ethosislami07@gmail.com>
2026-01-03 16:33:25 +0700

    EDIT Change Folder - ADD Route

--- backend/server.js
+++ backend/server.js
@@ -2,25 +2,28 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const productRoutes = require('./routes/productRoutes');
 
-dotenv.config()
+dotenv.config();
 
-// init app
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
+
 mongoose.connect(process.env.MONGO_URI)
-    .then(()=> console.log("MongoDB Connected Successfully"))
+    .then(() => console.log("MongoDB Connected Successfully"))
     .catch((err) => console.log("Error : ", err));
 
+
+app.use('/api/products', productRoutes);
 
 app.get('/', (req, res) => {
   res.send('Server is up and running!');
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
