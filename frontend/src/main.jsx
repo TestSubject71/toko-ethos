773fa1cba49970b2fa027e041d46822ab3995495
TestSubject71 <ethosislami07@gmail.com>
2026-01-23 20:44:49 +0700

    ADD - Log Rocket

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import LogRocket from 'logrocket';
+
+LogRocket.init('tjeig3/toko-ethos');
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
